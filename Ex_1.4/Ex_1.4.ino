1c0c77489137ab7094b14f72838ad967076588ed
Albertpuig2017 <albert.puig.20@lacetania.cat>
2018-05-26 06:04:01 +0200

    ex 1.4

---
+++ Ex_1.4/Ex_1.4.ino
@@ -0,0 +1,32 @@
+
+/**************************************************************************************
+**                                                                                   **
+**                           Comunicació Sèrie Bàsca                                 **
+**                                   Ex 1.4                                          **
+**                                                                                   **
+**    Nom:Albert Puig                                           Data: XX/XX/2018     **
+**************************************************************************************/
+//**********************************INCLUDE********************************************
+
+
+
+//********************************VARIABLES********************************************
+//************************************SETUP********************************************
+
+void setup() {                                     
+ Serial.begin(9600);       
+ Serial.println ("Escull el numero de l'operació que vols realitzar");
+ Serial.println ("1.Comprovar numero de tarjeta de credit");
+ Serial.println ("2. Comprovar numero de compte bancari");
+ Serial.println ("3. Buscar un digit perdut de tarjeta de credit");
+}
+//*******************************************LOOP**************************************
+void loop() {                                          //Inicia el bucle del programa
+  
+  
+
+  
+ 
+}
+
+//*************************************FUNCIONS****************************************
